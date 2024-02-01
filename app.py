59753e22ee5df98283026cbaf53e2106105da8c4
Naeem Raza <naee.raza@zeropoint.hr>
2024-02-01 17:17:08 +0500

    This is the changes in App.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return "Hello, World! This is the CICD using GitHub Actions" # added a line
 
 class TestApp(unittest.TestCase):
     def setUp(self):
